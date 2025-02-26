84ea76a933128228d47220b0c6d596ed22302800
Fode Oulare <foulare@e2r2p10.1337.ma>
2025-02-26 22:55:07 +0000

    fixed some problems

--- p2/script/provision.sh
+++ p2/script/provision.sh
@@ -1,5 +1,10 @@
+#!/bin/bash
+
 sudo apt-get update && sudo apt-get upgrade -y
-curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110
+
+curl -sfL https://get.k3s.io | sh -s - --node-ip=192.168.56.110 --tls-san=192.168.56.110
+
+sleep 30
 
 kubectl apply -f /config/app1.yaml
 kubectl apply -f /config/app2.yaml
